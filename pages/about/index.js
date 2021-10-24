7024140bd9a6bf43b9dde8ed66e54357f34bce22
keithchasen <keithchasen89@gmail.com>
2021-10-24 21:31:00 +0300

    head next element

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,16 @@
 import Router from 'next/router'
+import Head from 'next/head'
+
 export default function Index() {
   const clickBack = () => {
     Router.push('/')
   };
   return <>
+  <Head>
+    <title>About</title>
+    <meta name="keywords" content="about page"/>
+  </Head>
     <h1>About</h1>
     <button onClick={clickBack}>Back</button>
-  </>
-}
+    </>
+};
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function Home() {
   return <>
+    <Head>
+      <title>My App</title>
+      <meta name="keywords" content="main page"/>
+    </Head>
     <h1>Hello Next</h1>
     <Link href="/posts">Posts</Link>
     <Link href="/about">About</Link>
