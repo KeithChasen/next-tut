b3f8736a6ba84dfd0e6527ad0262c338f4c1ab2b
keithchasen <keithchasen89@gmail.com>
2021-10-24 21:19:57 +0300

    next links

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,10 @@
-export default function About() {
-  return <h1>About</h1>
+import Router from 'next/router'
+export default function Index() {
+  const clickBack = () => {
+    Router.push('/')
+  };
+  return <>
+    <h1>About</h1>
+    <button onClick={clickBack}>Back</button>
+  </>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1>Hello Next</h1>
+  return <>
+    <h1>Hello Next</h1>
+    <Link href="/posts">Posts</Link>
+    <Link href="/about">About</Link>
+  </>
 }
