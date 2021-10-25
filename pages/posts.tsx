55d1b58ff85c3b9c1b47667901fe8a4e517f088e
keithchasen <keithchasen89@gmail.com>
2021-10-25 14:01:08 +0300

    posts page in a TS way

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,8 +1,14 @@
 import {MainLayout} from "../components/MainLayout";
 import Link from "next/link";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import { MyPost } from "../interfaces/post";
+import {NextPageContext} from "next";
 
-export default function Posts({ posts: serverPosts }) {
+interface PostsPageProps {
+  posts: MyPost[]
+}
+
+export default function Posts({ posts: serverPosts }: PostsPageProps) {
   const [posts, setPosts] = useState(serverPosts);
   useEffect(() => {
     async function load() {
@@ -40,14 +46,14 @@ export default function Posts({ posts: serverPosts }) {
   )
 }
 
-Posts.getInitialProps = async ({ req }) => {
+Posts.getInitialProps = async ({ req }: NextPageContext) => {
   if (!req) {
     return {
       posts: null
     }
   }
   const response = await fetch('http://localhost:4200/posts');
-  const posts = await response.json();
+  const posts: MyPost[] = await response.json();
   return {
     posts
   }
