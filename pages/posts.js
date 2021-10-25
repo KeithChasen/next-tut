f746b61d16105e72c182ef5377d5e32505592c21
keithchasen <keithchasen89@gmail.com>
2021-10-25 12:16:13 +0300

    fetching posts in default react way

--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,23 @@
 import {MainLayout} from "../components/MainLayout";
+import { useState,useEffect } from 'react';
 
 export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function  load() {
+      const response = await fetch('http://localhost:4200/posts');
+      const json = await response.json();
+      setPosts(json);
+    }
+
+    load();
+  },[]);
   return (
     <MainLayout title="Posts" keywords="posts page">
       <h1>Posts</h1>
+      <ul>
+        <pre>{JSON.stringify(posts, null, 2)}</pre>
+      </ul>
     </MainLayout>
   )
 }
