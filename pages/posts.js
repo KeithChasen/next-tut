6867a01225b1e1318e1e49ab9e6c0609b0a9ec28
keithchasen <keithchasen89@gmail.com>
2021-10-25 12:29:13 +0300

    using getIntitialProps for loading posts in SSR way

--- pages/posts.js
+++ pages/posts.js
@@ -1,23 +1,27 @@
 import {MainLayout} from "../components/MainLayout";
-import { useState,useEffect } from 'react';
+import Link from "next/link";
 
-export default function Posts() {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    async function  load() {
-      const response = await fetch('http://localhost:4200/posts');
-      const json = await response.json();
-      setPosts(json);
-    }
-
-    load();
-  },[]);
+export default function Posts({ posts }) {
   return (
     <MainLayout title="Posts" keywords="posts page">
       <h1>Posts</h1>
       <ul>
-        <pre>{JSON.stringify(posts, null, 2)}</pre>
+        {
+          posts.map(post => (
+            <li key={post.id}>
+              <Link href={`post/${post.id}`}>{post.title}</Link>
+            </li>
+          ))
+        }
       </ul>
     </MainLayout>
   )
 }
+
+Posts.getInitialProps = async () => {
+      const response = await fetch('http://localhost:4200/posts');
+      const posts = await response.json();
+      return {
+        posts
+      }
+};
