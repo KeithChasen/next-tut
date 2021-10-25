2110b62f40609ddcbc9ff0395e867f566f1a47c8
keithchasen <keithchasen89@gmail.com>
2021-10-25 13:00:05 +0300

    added posts list loading from FE in case loading from server failed

--- pages/posts.js
+++ pages/posts.js
@@ -1,7 +1,29 @@
 import {MainLayout} from "../components/MainLayout";
 import Link from "next/link";
+import {useEffect, useState} from "react";
 
-export default function Posts({ posts }) {
+export default function Posts({ posts: serverPosts }) {
+  const [posts, setPosts] = useState(serverPosts);
+  useEffect(() => {
+    async function load() {
+      const response = await fetch('http://localhost:4200/posts');
+      const posts = await response.json();
+      setPosts(posts);
+    }
+    if (!serverPosts) {
+      load();
+    }
+  },[]);
+
+  if (!posts) {
+    return (
+      <MainLayout>
+        <p>Loading...</p>
+      </MainLayout>
+    )
+  }
+
+
   return (
     <MainLayout title="Posts" keywords="posts page">
       <h1>Posts</h1>
@@ -18,10 +40,15 @@ export default function Posts({ posts }) {
   )
 }
 
-Posts.getInitialProps = async () => {
-      const response = await fetch('http://localhost:4200/posts');
-      const posts = await response.json();
-      return {
-        posts
-      }
+Posts.getInitialProps = async ({ req }) => {
+  if (!req) {
+    return {
+      posts: null
+    }
+  }
+  const response = await fetch('http://localhost:4200/posts');
+  const posts = await response.json();
+  return {
+    posts
+  }
 };
